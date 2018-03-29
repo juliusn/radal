2a1ccb184efda6a3d0e70a5f79526f7e42605365
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2018-03-29 11:41:27 +0300

    database + authentication

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-// const Cat = mongoose.model('cat', catSchema);
-app.listen(3000);
+const mongoose = require('mongoose');
+const result = dotenv.config();
+if (result.error) throw result.error;
 /*
-mongoose.connect(`mongodb://$process.env.DB_USER}:${process.env.DB_PASS}@$process.env.DB_HOST}:${process.env.DB_PORT}/cat`).then(() => {
-  console.log('Connected successfully.');
+
+mongoose.connect({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+}).then(() => {
+  console.log('CONNECTED!');
   app.listen(3000);
-});*/
+});
+*/
+
+mongoose.connect(
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/cat`).
+    then(() => {
+      console.log('Connected successfully.');
+      app.listen(3000);
+    });
