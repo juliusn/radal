0be0476364b6b3d4979799f05209974262a9c4a2
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2018-04-27 14:14:08 +0300

    env/dep port optimization, debugging tools

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const logger = require('morgan');
+const debug = require('debug')('http');
 const http = require('http');
+const https = require('https');
 const app = express();
 const mongoose = require('mongoose');
+const fs = require('fs');
+const sslkey = fs.readFileSync('ssl-key.pem');
+const sslcert = fs.readFileSync('ssl-cert.pem');
+const options = {
+  key: sslkey,
+  cert: sslcert,
+};
+const env = app.get('env');
 const result = dotenv.config();
 if (result.error) throw result.error;
 
 app.use(logger('dev'));
 
-app.enable('trust proxy');
+if (!env) app.enable('trust proxy');
 
 app.use((req, res, next) => {
   if (req.secure) {
@@ -26,9 +36,12 @@ app.get('/', (req, res) => {
 mongoose.connect(
     `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/radal`).
     then(() => {
-      console.log('Database connected successfully.');
-      http.createServer(app).listen(3000);
-      console.log('Listening 3000.');
+      let httpPort = !env ? 80 : 8080;
+      let httpsPort = !env ? 443 : 4433;
+      http.createServer(app).listen(httpPort);
+      debug('HTTP server up listening ' + httpPort);
+      https.createServer(options, app).listen(httpsPort);
+      debug('HTTPS server up listening ' + httpsPort);
     }, (err) => {
-      console.log(err.message);
+      debug(err.message);
     });
