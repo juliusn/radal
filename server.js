ddf72afa87ec65d088d394b2d2fad32180caf843
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2018-04-27 15:22:23 +0300

    read certs only in dev

--- server.js
+++ server.js
@@ -6,20 +6,11 @@ const http = require('http');
 const https = require('https');
 const app = express();
 const mongoose = require('mongoose');
-const fs = require('fs');
-const sslkey = fs.readFileSync('ssl-key.pem');
-const sslcert = fs.readFileSync('ssl-cert.pem');
-const options = {
-  key: sslkey,
-  cert: sslcert,
-};
 const env = app.get('env');
 const result = dotenv.config();
 if (result.error) throw result.error;
 
 app.use(logger('dev'));
-
-
 
 app.get('/', (req, res) => {
   res.send('works');
@@ -28,9 +19,17 @@ app.get('/', (req, res) => {
 mongoose.connect(
     `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/radal`).
     then(() => {
+      debug('Database connected!');
       let httpPort = !env ? 80 : 8080;
       let httpsPort = !env ? 443 : 3000;
       if (env) {
+        const fs = require('fs');
+        const sslkey = fs.readFileSync('ssl-key.pem');
+        const sslcert = fs.readFileSync('ssl-cert.pem');
+        const options = {
+          key: sslkey,
+          cert: sslcert,
+        };
         https.createServer(options, app).listen(httpsPort);
         debug('HTTPS server up listening ' + httpsPort);
         http.createServer((req, res) => {
