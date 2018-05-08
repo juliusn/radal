a3a954c1e9d857e3e1a29da42d5e6a23e810bd45
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2018-05-09 00:18:47 +0300

    Prod redirect url

--- strategies/facebook.js
+++ strategies/facebook.js
@@ -15,7 +15,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new FacebookStrategy({
   clientID: '152386338928593',
   clientSecret: 'a4d1622d812f37109a2b980596a135bb',
-  callbackURL: 'https://localhost:8443/authenticate/authcallback',
+  callbackURL: 'https://radal.jelastic.metropolia.fi/authenticate/authcallback',
 }, (accessToken, refreshToken, profile, done) => {
   const query = {userid: profile.id};
   const doc = {
